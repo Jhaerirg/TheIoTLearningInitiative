84d1393a0de58e464c2ef0975478efd24c1711f5
Jaheri Reyes <jhaeryrg@gmail.com>
2016-07-13 21:41:59 +0000

    Internet of Things 101: 5.4.2 Protocols: Project:
    Minor Changes

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -2,14 +2,34 @@
 
 import paho.mqtt.client as paho
 import psutil
+import pywapi
 import signal
 import sys
 import time
 
 from threading import Thread
 
-def functionDataActuator():
-    print "Data Actuator"
+def functionApiWeather():
+    data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
+    message = data['location']['name']
+    message = message + ", Temperature " + data['current_conditions']['temperature'] + " C"
+    message = message + ", Atmospheric Pressure " + data['current_conditions']['barometer']['reading'][:-3] + " mbar"
+    return message
+
+def functionDataActuator(status):
+    print "Data Actuator Status %s" % status
+
+def functionDataActuatorMqttOnMessage(mosq, obj, msg):
+    print "Data Sensor Mqtt Subscribe Message!"
+    functionDataActuator(msg.payload)
+
+def functionDataActuatorMqttSubscribe():
+    mqttclient = paho.Client()
+    mqttclient.on_message = functionDataActuatorMqttOnMessage
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.subscribe("IoT101/DataActuator", 0)
+    while mqttclient.loop() == 0:
+        pass
 
 def functionDataSensor():
     netdata = psutil.net_io_counters()
@@ -39,9 +59,13 @@ if __name__ == '__main__':
     threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
     threadmqttpublish.start()
 
+    threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
+    threadmqttsubscribe.start()
+
     while True:
         print "Hello Internet of Things 101"
         print "Data Sensor: %s " % functionDataSensor()
+        print "API Weather: %s " % functionApiWeather()
         time.sleep(5)
 
 # End of File
