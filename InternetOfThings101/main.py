847455100dfb8ac98da0c5a9ffa7bf12845123d5
Jaheri Reyes <jhaeryrg@gmail.com>
2016-07-18 20:19:11 +0000

    Internet of Things 101: Challenge: Project: 04, 05, 06
    Data Reporting, Cloud Computing Services, freeboard.io & dweet.io
    adafruit.io
    IBM Watson IoT Platform

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -6,21 +6,73 @@ import pywapi
 import signal
 import sys
 import time
+import dweepy
+import client as mqtt
+import json
+import uuid
 
 from threading import Thread
 
 from flask import Flask
 from flask_restful import Api, Resource
 
+#----------------------------------------------
+macAddress = hex(uuid.getnode())[2:-1]
+macAddress = format(long(macAddress, 16),'012x')
+#remind the user of the mac address further down in code (post 'connecitng to QS')
+
+#Set the variables for connecting to the Quickstart service
+organization = "quickstart"
+deviceType = "iotsample-gateway"
+broker = ""
+topic = "iot-2/evt/status/fmt/json"
+username = ""
+password = ""
+
+error_to_catch = getattr(__builtins__,'FileNotFoundError', IOError)
+
+try:
+
+	file_object = open("./device.cfg")
+	
+	for line in file_object:
+				
+			readType, readValue = line.split("=")
+			
+			if readType == "org":	
+					organization = readValue.strip()
+			elif readType == "type": 
+					deviceType = readValue.strip()
+			elif readType == "id": 
+					macAddress = readValue.strip()
+			elif readType == "auth-method": 
+					username = "use-token-auth"
+			elif readType == "auth-token": 
+					password = readValue.strip()
+			else:
+					print("please check the format of your config file") #will want to repeat this error further down if their connection fails?
+		
+	file_object.close()
+										
+	print("Configuration file found - connecting to the registered service")
+		
+except error_to_catch:
+	print("No config file found, connecting to the Quickstart service")
+	print("MAC address: " + macAddress)
+
+#----------------------------------------------
+
 DeviceID="SensorDeviceJR"
 
 app = Flask(__name__)
 api = Api(app)
 
-class SensorRestApi(Resource):
+class DataSensorRestApi(Resource):
     def get(self): 
         data = 'Data from a (simulated) sensor'
         return data
+
+api.add_resource(DataSensorRestApi, '/sensor')
 
 def functionApiWeather():
     data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
@@ -44,12 +96,7 @@ def functionDataActuatorMqttSubscribe():
     while mqttclient.loop() == 0:
         pass
 
-api.add_resource(SensorRestApi, '/sensor')
-
 def functionDataSensor():
-    app.run('0.0.0.0', debug=True)
-
-def functionDataSensorJR():
     netdata = psutil.net_io_counters()
     data = netdata.packets_sent + netdata.packets_recv
     return data
@@ -62,10 +109,40 @@ def functionDataSensorMqttPublish():
     mqttclient.on_publish = functionDataSensorMqttOnPublish
     mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
-        data = functionDataSensorJR()
+        data = functionDataSensor()
         topic = "IoT101/"+DeviceID+"/DataSensor"
         mqttclient.publish(topic, data)
         time.sleep(1)
+
+#--------------------------------------------------------------------
+def functionIBMWatsonIoT():
+    #Creating the client connection
+    #Set clientID and broker
+    clientID = "d:" + organization + ":" + deviceType + ":" + macAddress
+    broker = organization + ".messaging.internetofthings.ibmcloud.com"
+
+    mqttc = mqtt.Client(clientID)
+
+    #Set authentication values, if connecting to registered service
+    if username is not "":
+	mqttc.username_pw_set(username, password=password)
+
+    mqttc.connect(host=broker, port=1883, keepalive=60)
+
+    #Publishing to IBM Internet of Things Foundation
+    mqttc.loop_start() 
+
+    while mqttc.loop() == 0:
+	msg = json.JSONEncoder().encode({"d":{"datasensor":functionDataSensor()}})	
+	mqttc.publish(topic, payload=msg, qos=0, retain=False)
+	print "message published"
+	time.sleep(5)
+	pass
+
+def functionServicesDweet():
+    dweepy.dweet_for('InternetOfThings101Dweet', {'DataSensor':functionDataSensor()})
+
+#--------------------------------------------------------------------
 
 def functionSignalHandler(signal, frame):
     sys.exit(0)
@@ -74,17 +151,24 @@ if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, functionSignalHandler)
 
+    # app.run(host='0.0.0.0', debug=True)
+    
     threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
     threadmqttpublish.start()
 
     threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
     threadmqttsubscribe.start()    
-        
+
+    #-----------------------------------------------------------------
+    threadibmwatsoniot = Thread(target=functionIBMWatsonIoT)
+    threadibmwatsoniot.start()
+    #-----------------------------------------------------------------        
+
     while True:
         print "Hello Internet of Things 101"
         # print "Data Sensor: %s " % functionDataSensor()        
-        # functionDataSensor()
         print "API Weather: %s " % functionApiWeather()
+        functionServicesDweet()
         time.sleep(5)
 
 # End of File
